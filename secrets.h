84d3d4cc4b1c15a55ca481eef4695440ca219751
YazenSH <87319592+YazenSH@users.noreply.github.com>
2025-02-13 11:32:49 +0300

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,16 @@
+// secrets_example.h
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi credentials
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+// MQTT broker settings
+const char* mqttServer = "YOUR_MQTT_SERVER";
+const int mqttPort = YOUR_MQTT_PORT;
+const char* mqttUser = "YOUR_MQTT_USERNAME";
+const char* mqttPassword = "YOUR_MQTT_PASSWORD";
+const char* mqttTopic = "YOUR_MQTT_TOPIC";
+
+#endif // SECRETS_H
